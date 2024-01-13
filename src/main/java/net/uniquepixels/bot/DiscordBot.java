c0a90c5b4d2c9f6172be1c9bcbed0fa9252d1365
shorty <ungegrieftfirma@gmail.com>
2024-01-13 16:09:30 +0100

    feat(other builders): so many new builders yay

--- src/main/java/net/uniquepixels/bot/DiscordBot.java
+++ src/main/java/net/uniquepixels/bot/DiscordBot.java
@@ -5,7 +5,11 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.uniquepixels.bot.discord.builder.commands.handler.CommandListener;
+import net.uniquepixels.bot.discord.button.handler.ButtonListener;
+import net.uniquepixels.bot.discord.commands.handler.CommandListener;
+import net.uniquepixels.bot.discord.menu.entity.handler.EntityMenuListener;
+import net.uniquepixels.bot.discord.menu.string.handler.StringMenuListener;
+import net.uniquepixels.bot.discord.modal.handler.ModalListener;
 import net.uniquepixels.bot.mongo.MongoConnection;
 
 public class DiscordBot extends ListenerAdapter {
@@ -15,7 +19,7 @@ public class DiscordBot extends ListenerAdapter {
 
         JDABuilder builder = JDABuilder.createDefault(System.getenv("DISCORD_TOKEN"));
 
-        builder.addEventListeners(new CommandListener());
+        builder.addEventListeners(new CommandListener(), new ButtonListener(), new ModalListener(), new StringMenuListener(), new EntityMenuListener());
 
         builder.setActivity(Activity.watching("Discord UP Template"));
 
